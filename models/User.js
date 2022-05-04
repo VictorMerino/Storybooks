82fca6bdfb14a652741eab30650a38d5341aee9f
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-04 23:33:51 +0200

    Separate user model (as google and local ones does not seems actually similar)

--- models/User.js
+++ models/User.js
@@ -1,5 +1,33 @@
 import mongoose from 'mongoose'
 
+/* 
+Google+ Schema
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}) */
+
 const UserSchema = new mongoose.Schema({
   googleId: {
     type: String,
@@ -25,5 +53,4 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 })
-
 export default mongoose.model('User', UserSchema)
