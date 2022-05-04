2b5507e1a8d03737458011fe8c2f7933e11f8c3e
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-04 09:40:25 +0200

    Passport config: we're moving in the right direction, but still not finished

--- config/passport.js
+++ config/passport.js
@@ -1,16 +1,27 @@
-import { Strategy as GoogleStrategy, passport } from 'passport-google-oauth20'
+import passport from 'passport'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+/* import passport from 'passport-google-oauth20'
+const { Strategy: GoogleStrategy } = passport */
+import dotenv from 'dotenv'
+dotenv.config()
+
+import mongoose from 'mongoose'
+import User from '../models/User.js'
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://www.example.com/auth/google/callback',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
     },
-    (accessToken, refreshToken, profile, cb) => {
+    async (accessToken, refreshToken, profile) => {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
         return cb(err, user)
       })
+      // Here more functions?
     }
   )
 )
+
+export default passport
