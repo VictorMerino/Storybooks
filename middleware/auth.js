c079862052e9fc3eca2d8420ba87c808b94d771d
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-04 15:54:20 +0200

    Add middleware to redirect to correct view when logged in (or not)

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+export const ensureAuth = (req, res, next) => {
+  console.log('req.isAuthenticated', req.isAuthenticated())
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    res.redirect('/')
+  }
+}
+
+export const ensureGuest = (req, res, next) => {
+  console.log('req.isAuthenticated', req.isAuthenticated())
+  if (req.isAuthenticated()) {
+    res.redirect('/dashboard')
+  } else {
+    return next()
+  }
+}
