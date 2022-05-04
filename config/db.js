881ab822961291585031eee40707845fa23a98af
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-04 11:21:35 +0200

    Just a TO-DO to be checked before production

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 export const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
+      // TO-DO: they seems deprecated, remove when checked it
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
       // useFindAndModify: true,
