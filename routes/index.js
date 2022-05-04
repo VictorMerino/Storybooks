6fa691cc34201ef77a6eb75f40714fb797a60580
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-04 15:52:30 +0200

    Add middleware to redirect to correct view when logged in (or not)

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 import express from 'express'
 
+import { ensureAuth, ensureGuest } from '../middleware/auth.js'
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
   res.render('login', {
     layout: 'login',
   })
 })
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
   res.render('dashboard')
 })
 
