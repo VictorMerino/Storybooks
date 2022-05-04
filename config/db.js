22f6e718a11b49471cda7f8803a11165984326db
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-04 10:04:05 +0200

    These options are no longer needed

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 export const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
       // useFindAndModify: true,
     })
 
