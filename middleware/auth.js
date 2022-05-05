9168bcf169a914141c52744b601a036941e4e784
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-06 00:35:53 +0200

    Add flash message for not-logged redirect

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,7 @@ export const ensureAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next()
   } else {
+    req.flash('errorMsg', 'You need to be logged in to see this resource')
     res.redirect('/')
   }
 }
