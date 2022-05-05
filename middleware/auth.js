865590a47fc21961a683be4cd151df6d50e43333
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-05 23:42:24 +0200

    Get back the middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 export const ensureAuth = (req, res, next) => {
   // console.log('req.isAuthenticated', req.isAuthenticated())
   // console.log(req.user)
-  // if (req.isAuthenticated()) {
-  return next()
-  /*   } else {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
     res.redirect('/')
-  } */
+  }
 }
 
 export const ensureGuest = (req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,18 @@
 import express from 'express'
 
-// import { ensureAuth, ensureGuest } from '../middleware/auth.js'
+import { ensureAuth, ensureGuest } from '../middleware/auth.js'
 
 const router = express.Router()
 
-router.get(
-  '/',
-  /* ensureGuest, */ (req, res) => {
-    res.render('login', {
-      layout: 'login',
-    })
-  }
-)
+router.get('/', ensureGuest, (req, res) => {
+  res.render('login', {
+    layout: 'login',
+  })
+})
 
-router.get(
-  '/dashboard',
-  /* ensureAuth, */ (req, res) => {
-    console.log(req.user)
-    res.render('dashboard')
-  }
-)
+router.get('/dashboard', ensureAuth, (req, res) => {
+  console.log(req.user)
+  res.render('dashboard')
+})
 
 export default router
