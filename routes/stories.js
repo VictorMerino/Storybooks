20d7b37d6d4794b55f7b90cf19d74611e3cf2a4a
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-07 15:37:51 +0200

    Edit story endpoint

--- routes/stories.js
+++ routes/stories.js
@@ -19,6 +19,7 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
     }).lean()
     console.log(story)
     if (!story) return res.render('error/404')
+    // TO-DO: check for possible code-smell: I'm copying the next conditional:
     if (canEdit(story.user, req.user.id)) {
       console.log(story)
       res.render('stories/edit', { story })
@@ -61,6 +62,47 @@ router.post('/', ensureAuth, async (req, res) => {
       await Story.create(newStory)
       res.redirect('/dashboard')
     }
+  } catch (err) {
+    res.render('error/500')
+    throw new Error(err)
+  }
+})
+
+router.put('/edit/:id', ensureAuth, async (req, res) => {
+  try {
+    const { title, status, body } = req.body
+
+    let story = await Story.findById(req.params.id).lean()
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    let errors = []
+    console.log('Title ', title)
+    if (!title || !status || !body) {
+      errors.push({ msg: 'All fields are required' })
+    }
+
+    if (errors.length) {
+      console.log('Check for errors', errors)
+      return res.render('stories/edit', { story, errors })
+    }
+
+    if (canEdit(story.user, req.user.id)) {
+      story = await Story.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      }).lean()
+      res.redirect('/dashboard')
+    } else {
+      console.log(
+        'Something strange. User is trying to modify a story from other user'
+      )
+      req.flash('errorMsg', 'You cannot edit this story')
+      res.render('error/500')
+    }
+
+    return false
   } catch (err) {
     res.render('error/500')
     throw new Error(err)
