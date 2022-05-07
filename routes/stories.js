b708b4340efb56b8837c8ace77ea2620e7032cba
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-07 13:58:41 +0200

    Edit story: template and route

--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,8 @@
 import express from 'express'
 
 import { ensureAuth } from '../middleware/auth.js'
+import { canEdit } from '../helpers/hbs.js'
+
 import Story from '../models/Story.js'
 import User from '../models/User.js'
 
@@ -8,6 +10,27 @@ const router = express.Router()
 
 router.get('/add', ensureAuth, (req, res) => {
   res.render('stories/add')
+})
+
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean()
+    console.log(story)
+    if (!story) return res.render('error/404')
+    if (canEdit(story.user, req.user.id)) {
+      console.log(story)
+      res.render('stories/edit', { story })
+    } else {
+      console.log('Peta en el else')
+      req.flash('errorMsg', 'You cannot edit this story')
+      res.redirect('/')
+    }
+  } catch (err) {
+    console.log(err)
+    res.render('error/500')
+  }
 })
 
 router.post('/', ensureAuth, async (req, res) => {
