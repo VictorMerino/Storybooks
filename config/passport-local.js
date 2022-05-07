37ba6f778a27c70d7becfacb4a29af8248966b2b
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-08 00:32:52 +0200

    Dead code

--- config/passport-local.js
+++ config/passport-local.js
@@ -1,6 +1,5 @@
 import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
-import mongoose from 'mongoose'
 import bcrypt from 'bcryptjs'
 
 import User from '../models/User.js'
